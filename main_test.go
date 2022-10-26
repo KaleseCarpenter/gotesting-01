116064610bc9a8be8ae000a9c1b8fda1b401ea2b
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-10-26 14:35:49 -0400

    added test for array of structs

--- main_test.go
+++ main_test.go
@@ -7,3 +7,22 @@ func TestCalculate(t *testing.T) {
 		t.Error("Expected 2 + 2 to equal 4")
 	}
 }
+
+func TestTableCalculate(t *testing.T) {
+	// an array of type struct
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected {
+			t.Error("Test Failed: {} inputted, {} expected, receieved: {}", test.input, test.expected, output)
+		}
+	}
+}
